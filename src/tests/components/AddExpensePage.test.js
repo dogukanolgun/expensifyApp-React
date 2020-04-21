dd5ae06c751edba6afee276f644a625e08baf122
Dogukan Olgun <dogukanolgun20000@gmail.com>
2020-04-21 07:28:45 +0300

    Setup devDependencies and dist folder

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,22 +3,20 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let addExpenseSpy, historySpy, wrapper;
+let addExpense, history, wrapper;
 
 beforeEach(() => {
-    addExpenseSpy = jest.fn();
-    historySpy = { push: jest.fn() };
-    wrapper = shallow(
-        <AddExpensePage addExpense={addExpenseSpy} history={historySpy} />
-    );
+  addExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
 });
 
 test('should render AddExpensePage correctly', () => {
-    expect(wrapper).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-    expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(addExpenseSpy).toHaveBeenLastCalledWith(expenses[1]);
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,39 +3,36 @@ import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 
-let editExpenseSpy, removeExpenseSpy, historySpy, wrapper;
+let editExpense, removeExpense, history, wrapper;
 
 beforeEach(() => {
-    editExpenseSpy = jest.fn();
-    removeExpenseSpy = jest.fn();
-    historySpy = { push: jest.fn() };
-    wrapper = shallow(
-        <EditExpensePage
-            editExpense={editExpenseSpy}
-            removeExpense={removeExpenseSpy}
-            history={historySpy}
-            expense={expenses[2]}
-        />
-    );
+  editExpense = jest.fn();
+  removeExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(
+    <EditExpensePage
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+      expense={expenses[2]}
+    />
+  );
 });
 
 test('should render EditExpensePage', () => {
-    expect(wrapper).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle editExpense', () => {
-    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
-    expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(editExpenseSpy).toHaveBeenLastCalledWith(
-        expenses[2].id,
-        expenses[2]
-    );
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
 test('should handle removeExpense', () => {
-    wrapper.find('button').simulate('click');
-    expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpenseSpy).toHaveBeenLastCalledWith({
-        id: expenses[2].id
-    });
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({
+    id: expenses[2].id
+  });
 });
