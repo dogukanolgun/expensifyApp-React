48b441a461fc5e7b40fa1c6ccf3f007975f7856f
Dogukan Olgun <dogukanolgun20000@gmail.com>
2020-04-21 06:06:55 +0300

    1 test doesnt work. I am going to compare it to the course's source code

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-    <div>
-        <Link to={`/edit/${id}`}>
-            <h3>{description}</h3>
-        </Link>
-        <p>{amount} - {createdAt}</p>
-    </div>
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {amount}-{moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
 );
 
 export default ExpenseListItem;
